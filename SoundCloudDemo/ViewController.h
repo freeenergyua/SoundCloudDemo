6ad56a69a8594cb4976f68e20edd36747db1cee5
iNailuY <inailuy@me.com>
2015-01-06 14:20:47 -0500

    Initial Commit

---
+++ SoundCloudDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoundCloudDemo
+//
+//  Created by inailuy on 1/6/15.
+//  Copyright (c) 2015 inailuy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
